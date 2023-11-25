33fb3b9c81b4b41d1e0579b39281acf9bd51227e
vraj <vrajgangani1818@gmail.com>
2023-11-25 20:23:44 +0530

    add drawer

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   EditOutlined,
   FileTextOutlined,
   ShareAltOutlined,
+  AlignLeftOutlined,
+  HomeOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, theme } from "antd";
+import {Drawer, Layout, Menu, theme } from "antd";
 import { Link, useNavigate } from "react-router-dom";
-import "../Components/Navbar.css"
+import "../Components/Navbar.css";
 import { AppRoute } from "../Router/AppRoute";
 import { AppName } from "../Assets/AppInfo";
-const { Content, Sider } = Layout;
+const { Content} = Layout;
 
 const Navbar = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [visible, setvisible] = useState(false);
 
   const {
     token: { colorBgContainer },
   } = theme.useToken();
 
   const NavItems = [
+    {
+      lable: "Home",
+      icon: <HomeOutlined />,
+      path: "/",
+    },
     {
       lable: "Share App",
       icon: <ShareAltOutlined />,
@@ -36,57 +44,69 @@ const Navbar = () => {
     },
   ];
   return (
-    <Layout style={{ height: "100%" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
+    <Layout>
+      <Drawer
+        visible={visible}
+        title={AppName}
+        closable={true}
+        maskClosable={true}
+        onClose={() => {
+          setvisible(false);
         }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-        // style={{ height: "100%" }}
+        placement="left"
+        headerStyle={{ padding: 10 }}
+        width={230}
+        bodyStyle={{padding:5}}
       >
-        <div className="demo-logo-vertical" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
+        <Menu mode="inline" defaultSelectedKeys={["4"]}>
           {NavItems.map((data, index) => (
-            <Menu.Item key={String(index + 1)} icon={data.icon}>
+            <Menu.Item
+              key={String(index + 1)}
+              icon={data.icon}
+              style={{ padding: 7}}
+            >
               <Link to={data.path}>{data.lable}</Link>
             </Menu.Item>
           ))}
         </Menu>
-      </Sider>
-      <Layout>
+      </Drawer>
+      <div
+        style={{
+          padding: "10px",
+          background: colorBgContainer,
+          fontWeight: "bold",
+          textAlign: "center",
+          cursor: "pointer",
+          position: "relative",
+        }}
+        onClick={() => navigate("/")}
+      >
+        <div style={{ position: "absolute" }}>
+          <AlignLeftOutlined
+            style={{padding:"0 0 0 7px"}}
+            onClick={() => {
+              setvisible(true);
+            }}
+          />
+        </div>
+        {AppName}
+      </div>
+      <Content
+        style={{
+          margin: "10px 10px 0",
+        }}
+      >
         <div
           style={{
-            padding: "10px",
+            padding: "0px 5px 0px 5px",
+            height: "100%",
             background: colorBgContainer,
-            fontWeight: "bold",
-            textAlign: "center",
-            cursor: "pointer",
+            borderRadius: "5px",
           }}
-          onClick={()=>navigate('/')}
         >
-          {AppName}
+          <AppRoute />
         </div>
-        <Content
-          style={{
-            margin: "10px 10px 0",
-          }}
-        >
-          <div
-            style={{
-              padding:"0px 5px 0px 5px",
-              height: "100%",
-              background: colorBgContainer,
-              borderRadius: "5px",
-            }}
-          >
-        <AppRoute/>
-          </div>
-        </Content>
-      </Layout>
+      </Content>
     </Layout>
   );
 };
